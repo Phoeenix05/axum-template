543c5a8688083ea22e90cdb5846a74f1851e0b87
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-02-03 17:35:45 +0200

    axum template, yay

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use axum::http::Method;
+use tower_http::cors::{Any, CorsLayer};
+use tower_http::trace::TraceLayer;
+
+#[tokio::main]
+async fn main() {
+    env_logger::builder().init();
+
+    let trace = TraceLayer::new_for_http();
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
+    let app = axum::Router::new()
+        // <routes here>
+        .layer(tower::ServiceBuilder::new().layer(trace).layer(cors));
+
+    let tcp_listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(tcp_listener, app).await.unwrap();
+}
